227c998838d7eaceedc40b6480c55a651c19569f
Jo Minji <mingi2687@gamil.com>
2023-05-05 15:37:35 +0900

    eslint, tsconfig, 설정, @wanted/ui package  생성

--- apps/wanted/next.config.js
+++ apps/wanted/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ['@wanted/ui'], // 추가
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
